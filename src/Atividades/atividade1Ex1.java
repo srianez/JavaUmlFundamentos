f681855eed1060a154ada03a7eb1507da56d1797
silasianez <silas.ianez@techne.com.br>
2017-05-15 09:18:00 -0300

    Inclusao de atividade 1

---
+++ src/Atividades/atividade1Ex1.java
@@ -0,0 +1,18 @@
+package Atividades;
+
+public class atividade1Ex1 {
+	
+	public static void main(String args[]) {
+
+		int[] x = {10,20,30};       //inicia o array com as 3 posições
+	    int y =  x.length;
+	    
+		for (int i = x.length -1; i >=0; i--) {
+			
+			System.out.println(x[i]);
+	  }
+		
+	}
+}
+
+
